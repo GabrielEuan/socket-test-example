88892e7420a1f6338393f15256f7b721b0a089e8
Gabriel Euan <euan.alberto415@gmail.com>
2021-02-05 13:53:38 -0600

    Added past project

--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+var messages = [{
+    author: "Carlos Padillo",
+    text: "hola",
+}];
+
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+
+    socket.emit('messages', messages);
+
+    socket.on('new-message', function(data) {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    })
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
